bf0f32da654a9e2b484f35d5415a56000e85ce49
BahadirAydin <bahadiraydin1923@gmail.com>
2024-04-28 00:09:04 +0300

    narrow bridge implementation

--- NarrowBridge.cpp
+++ NarrowBridge.cpp
@@ -1,1 +1,30 @@
 #include "NarrowBridge.h"
+#include <iostream>
+
+void NarrowBridge::enterBridge(int carID, int direction) {
+    __synchronized__;
+    std::queue<int> &currentQueue = (direction == 0 ? northQueue : southQueue);
+    std::queue<int> &oppositeQueue = (direction == 1 ? northQueue : southQueue);
+
+    if (currentQueue.empty() && oppositeQueue.empty()) {
+        this->currentDirection = direction;
+    }
+
+    currentQueue.push(carID);
+
+    while (this->currentDirection != direction) {
+        this->direction.wait();
+    }
+}
+
+void NarrowBridge::leaveBridge(int carID, int direction) {
+    __synchronized__;
+    std::queue<int> &currentQueue = (direction == 0 ? northQueue : southQueue);
+    if (!currentQueue.empty() && currentQueue.front() == carID) {
+        currentQueue.pop();
+    }
+    if (currentQueue.empty()) {
+        this->currentDirection = !this->currentDirection;
+        this->direction.notifyAll();
+    }
+}
--- NarrowBridge.h
+++ NarrowBridge.h
@@ -1,10 +1,20 @@
 #ifndef NARROWBRIDGE_H
 #define NARROWBRIDGE_H
 #include "monitor.h"
+#include <pthread.h>
+#include <queue>
 
 class NarrowBridge : public Monitor {
   public:
-    int travelTime{};
-    int maxWaitTime{};
+    int travel_time{};
+    int max_wait_time{};
+    std::queue<int> northQueue{};
+    std::queue<int> southQueue{};
+    Condition direction{this};
+    int currentDirection{0};
+
+    void enterBridge(int carID, int direction);
+    void leaveBridge(int carID, int direction);
+    void changeDirection();
 };
 #endif
