21102a474097dbd4e4df4ec960c6ed030f0bbcfc
BahadirAydin <bahadiraydin1923@gmail.com>
2024-04-28 03:04:04 +0300

    ta update

--- WriteOutput.h
+++ WriteOutput.h
@@ -1,15 +1,21 @@
 #ifndef HOMEWORK2_WRITEOUTPUT_H
 #define HOMEWORK2_WRITEOUTPUT_H
 
-#include <pthread.h>
 #include <stdio.h>
+
+#include <pthread.h>
 #include <sys/time.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef enum Action { TRAVEL, ARRIVE, START_PASSING, FINISH_PASSING } Action;
+typedef enum Action {
+    TRAVEL,
+    ARRIVE,
+    START_PASSING,
+    FINISH_PASSING
+}Action;
 
 void InitWriteOutput();
 unsigned long long GetTimestamp();
@@ -22,9 +28,9 @@ void PrintThreadId();
  * @param connectorID
  * @param action
  */
-void WriteOutput(int carID, char connector_type, int connectorID,
-                 Action action);
+void WriteOutputf(FILE *f, int carID, char connector_type, int connectorID, Action action);
+void WriteOutput(int carID, char connector_type, int connectorID, Action action);
 #ifdef __cplusplus
 }
 #endif
-#endif // HOMEWORK2_WRITEOUTPUT_H
+#endif //   HOMEWORK2_WRITEOUTPUT_H
